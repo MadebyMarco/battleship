228eeef2715e12d29ccba07733c129840fe9d0f4
MadebyMarco <marco3xmedina@gmail.com>
2023-10-13 14:08:16 -0700

    create factory, funcs: isSunk(),  hit()

---
+++ ship.js
@@ -0,0 +1,13 @@
+function ship(length) {
+  const numberOfHits = 0;
+
+  function hit() {
+    this.numberOfHits++;
+  }
+
+  function isSunk() {
+    if (this.numberOfHits >= this.length) return true;
+    return false;
+  }
+  return { length, numberOfHits, isSunk, hit };
+}
