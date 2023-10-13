21a636cc075a44a2cc2ec0ee6c5a907ac6246f94
MadebyMarco <marco3xmedina@gmail.com>
2023-10-13 16:34:55 -0700

    test: placeShip

---
+++ gameboard.test.js
@@ -0,0 +1,12 @@
+const Gameboard = require("./gameboard");
+const Ship = require("./ship");
+const gameboard = Gameboard();
+const ship = Ship(3);
+test("check gameboard ships and coordinates are being added correctly", () => {
+  expect(gameboard.ships).toEqual([]);
+  gameboard.placeShip([1, 1], [1, 2], [1, 3]);
+  expect(gameboard.ships.length).toEqual(1);
+  expect(gameboard.shipCoordinates.length).toEqual(1);
+  expect(gameboard.ships[0].length).toBe(ship.length);
+  expect(gameboard.shipCoordinates[0].length).toBe(ship.length);
+});
