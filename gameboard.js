f772d69c4e41aa23e655091cd8f7ac229a5d65ad
MadebyMarco <marco3xmedina@gmail.com>
2023-10-13 15:12:02 -0700

    pseudocode: explains roles of gameboard

---
+++ gameboard.js
@@ -0,0 +1,7 @@
+function Gameboard() {
+  // there will be two gameBoards. One for the player, one for the computer
+  //can place ships on the board at specific coordinates
+  //tracks coordinates
+  //determines which attacks are hits and misses, then sends the hit to the right ship or records the miss
+  //reports whether all ships of one side have been sunk
+}
