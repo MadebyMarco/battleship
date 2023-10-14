12b8825224551579050424fb2d1c60f50488482c
MadebyMarco <marco3xmedina@gmail.com>
2023-10-14 10:48:40 -0700

    func: receiveAttack

--- gameboard.js
+++ gameboard.js
@@ -15,19 +15,24 @@ function Gameboard() {
   }
   //determines which attacks are hits and misses, then sends the hit to the right ship or records the miss
   function receiveAttack(coordinate) {
-    return _isHit(this.shipCoordinates, coordinate);
+    const index = _isHit(this.shipCoordinates, coordinate);
+    if (index === false) {
+      this.misses.push(coordinate);
+      return;
+    }
+    this.ships[index].hit();
   }
 
   function _isHit(shipCoordinates, coordinate) {
     for (let shipIndex = 0; shipIndex < shipCoordinates.length; shipIndex++) {
-      // first loop will go over each COLLECTION of coordinates
-      //This index will be used to identify which ship will be hit.
+      //   // first loop will go over each COLLECTION of coordinates
+      //   //This index will be used to identify which ship will be hit.
       for (
         let coordinatesIndex = 0;
         coordinatesIndex < shipCoordinates[shipIndex].length;
         coordinatesIndex++
       ) {
-        //Second loop checks each pair of coordinates and compares their values to target coordinate
+        //     //Second loop checks each pair of coordinates and compares their values to target coordinate
         const currentCoordinate = shipCoordinates[shipIndex][coordinatesIndex];
         if (
           currentCoordinate[0] == coordinate[0] &&
