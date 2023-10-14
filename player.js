d42431a0721fad5ffb8c9e969c2a9638e979ce3f
MadebyMarco <marco3xmedina@gmail.com>
2023-10-14 12:38:57 -0700

    pseudocode: explains role of player

---
+++ player.js
@@ -0,0 +1,3 @@
+// create player factory that can take turns playing the game by attacking the enemy Gameboard;
+//create property called turn to set to false or true
+//make a player not capable of making the same move twice
