7f7be13fed8015f25563665a2a7109fc215120ea
MadebyMarco <marco3xmedina@gmail.com>
2023-10-14 13:29:32 -0700

    func: _isNovelMove

--- player.js
+++ player.js
@@ -1,3 +1,33 @@
 // create player factory that can take turns playing the game by attacking the enemy Gameboard;
 //create property called turn to set to false or true
 //make a player not capable of making the same move twice
+// make an ai that can attack the other gameboard
+function Player() {
+  let turn = false;
+
+  function setTurn(boolean) {
+    this.turn = boolean;
+  }
+
+  const moves = [];
+
+  function getRandomCoordinate(maxGridSize = 10) {
+    const coordinate0 = Math.floor(Math.random() * maxGridSize);
+    const coordinate1 = Math.floor(Math.random() * maxGridSize);
+    return [coordinate0, coordinate1];
+  }
+
+  function _isNovelMove(move) {
+    let novelStatus = true;
+    moves.map((oldMove) => {
+      if (oldMove[0] == move[0] && oldMove[1] == move[1]) novelStatus = false;
+    });
+    return novelStatus;
+  }
+
+  function getAttack() {}
+
+  return { turn, setTurn, moves, _isNovelMove };
+}
+
+module.exports = Player;
