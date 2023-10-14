ba63b0b91d9d54feb4d1afc8d33c6df0b4516e10
MadebyMarco <marco3xmedina@gmail.com>
2023-10-14 14:00:35 -0700

    test: getAttack

--- player.test.js
+++ player.test.js
@@ -3,6 +3,5 @@ const player = Player();
 player.moves.push([1, 1]);
 
 test("random attack by computer", () => {
-  expect(player.isNovelMove([1, 1])).toEqual(false);
-  expect(player.isNovelMove([1, 2])).toEqual(true);
+  expect(player.getAttack()).toBeTruthy();
 });
