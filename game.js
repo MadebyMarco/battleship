48f4268224053f52d4ec485af55c3bfc0145c317
MadebyMarco <marco3xmedina@gmail.com>
2023-10-15 20:21:09 -0700

    func: isOver

--- game.js
+++ game.js
@@ -5,12 +5,25 @@
 // 3.the game loop should step through the game turn by turn using only methods from other objects?
 // 3.1 i think this means that the game loop function is a high-order function that should only be executing other functions
 // 4. create base case to end loop once all of one player's ships have been sunk in the game module
+function Game() {
+  const Player = require("./player");
+  const Gameboard = require("./gameboard");
 
-const Player = require("./player");
-const Gameboard = require("./gameboard");
+  const player1 = Player();
+  const player2 = Player();
 
-const player1 = Player();
-const player2 = Player();
+  player1.gameboard = Gameboard();
+  player2.gameboard = Gameboard();
 
-const player1Gameboard = Gameboard();
-const player2Gameboard = Gameboard();
+  function start() {}
+
+  function isOver() {
+    if (
+      player1.gameboard.areAllShipsSunk() ||
+      player2.gameboard.areAllShipsSunk()
+    ) {
+      return true;
+    }
+    return false;
+  }
+}
