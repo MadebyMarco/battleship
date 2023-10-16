163c019253b0c66e1aa0c723166aee1cf3452f93
MadebyMarco <marco3xmedina@gmail.com>
2023-10-15 20:00:48 -0700

    psuedocode: game loop rules
    completed first step

---
+++ game.js
@@ -0,0 +1,16 @@
+// create game loop
+// 1. creates players and gameboards - place fake ships for now
+// 2. displays player boards and render them using information from the gameboard objects
+// 2.1 need methods to render the gameboard and to take user input for attacking, ex: clicking on a square, taking its coordinate, attacking enemy gameboard
+// 3.the game loop should step through the game turn by turn using only methods from other objects?
+// 3.1 i think this means that the game loop function is a high-order function that should only be executing other functions
+// 4. create base case to end loop once all of one player's ships have been sunk in the game module
+
+const Player = require("./player");
+const Gameboard = require("./gameboard");
+
+const player1 = Player();
+const player2 = Player();
+
+const player1Gameboard = Gameboard();
+const player2Gameboard = Gameboard();
