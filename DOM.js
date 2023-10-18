cfb4fd73f01b553c97592bb90bfb3e83e4851f2e
MadebyMarco <marco3xmedina@gmail.com>
2023-10-18 14:00:41 -0700

    func: placeShip

--- DOM.js
+++ DOM.js
@@ -22,13 +22,27 @@ function DOM() {
     if (result == "hit") element.classlist.add("hit");
     if (result == "miss") element.classlist.add("miss");
   }
+  // stern: back of boat
+  // bow: front of boat
+  function placeShip(isVertical, ...elements) {
+    let orientation = "horizontal";
+    if (isVertical) orientation = "vertical";
+    for (let index = 0; index < elements.length; index++) {
+      if (index == 0) elements[index].classList.add("stern");
+      if (index == elements.length - 1) elements[index].classList.add("bow");
+      elements[index].classList.add(orientation);
+      elements[index].classList.add("ship");
+    }
+  }
 
   function initialize() {
     const gameboard1 = document.querySelector("#player1.gameboard");
     gameboard1.append(...createGameboard(gameboardSettings));
-    const gameboard2 = document.querySelector("#player1.gameboard");
+    const gameboard2 = document.querySelector("#player2.gameboard");
     gameboard2.append(...createGameboard(gameboardSettings));
   }
 
-  return { createElements, createGameboard, receiveAttack, initialize };
+  return { receiveAttack, initialize, placeShip };
 }
+
+DOM().initialize();
