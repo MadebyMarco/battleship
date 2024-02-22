87bc683cfe96c29c8dcf4a47a066ecd68637726e
MadebyMarco <marco3xmedina@gmail.com>
2024-02-22 12:10:45 -0800

    todo: move onclick to UI

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-import { DOM } from "./DOM";
-import { Game } from "./game";
-import { Player } from "./player";
-import { Ship } from "./ship";
+import { DOM } from "./DOM.js";
+import { Game } from "./game.js";
+// import { Player } from "./player";
+// import { Ship } from "./ship";
+import { UI } from "./UI.js";
+
+const game = Game();
+const dom = DOM();
+game.player2.ai = true;
+// dom.initializeGame();
+document.querySelector(".startButton").onclick = () => {
+  dom.renderPlayerGameboard([], "player1");
+  dom.renderModelShips("player1");
+};
+UI(game.player1, game.player2);
