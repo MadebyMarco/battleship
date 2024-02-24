595fab3e8f95959d67d9747bad5ce029d354332c
MadebyMarco <marco3xmedina@gmail.com>
2024-02-24 11:59:28 -0800

    move code to ui.js

--- index.js
+++ index.js
@@ -8,8 +8,4 @@ const game = Game();
 const dom = DOM();
 game.player2.ai = true;
 // dom.initializeGame();
-document.querySelector(".startButton").onclick = () => {
-  dom.renderPlayerGameboard([], "player1");
-  dom.renderModelShips("player1");
-};
 UI(game.player1, game.player2);
