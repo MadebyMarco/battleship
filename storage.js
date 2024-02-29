2c013f8aa3154a2f4dae11387285355e8d57ef48
MadebyMarco <marco3xmedina@gmail.com>
2024-02-29 12:11:21 -0800

    JSON stringify objects

--- storage.js
+++ storage.js
@@ -1,10 +1,10 @@
 export function Storage() {
   function setPlayer(player = {}, playerNumber = 1) {
-    localStorage.setItem("player" + playerNumber, player);
+    localStorage.setItem("player" + playerNumber, JSON.stringify(player));
   }
 
   function setGameboard(gameboard = {}, playerNumber = 1) {
-    localStorage.setItem("gameboard" + playerNumber, gameboard);
+    localStorage.setItem("gameboard" + playerNumber, JSON.stringify(gameboard));
   }
 
   function getPlayer(playerNumber = 1) {
@@ -16,7 +16,10 @@ export function Storage() {
   }
 
   function setCoordinates(playerNumber, coordinates) {
-    localStorage.setItem("player" + playerNumber + "Coordinates", coordinates);
+    localStorage.setItem(
+      "player" + playerNumber + "Coordinates",
+      JSON.stringify(coordinates)
+    );
   }
 
   function getCoordinates(playerNumber) {
