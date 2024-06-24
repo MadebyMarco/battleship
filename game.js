7bed96fb1ec61aaae3663e2c73aedbbea0d5e734
MadebyMarco <marco3xmedina@gmail.com>
2024-06-24 12:19:32 -0700

    remove player and gameboard imports

--- game.js
+++ game.js
@@ -5,29 +5,7 @@
 // 3.the game loop should step through the game turn by turn using only methods from other objects?
 // 3.1 i think this means that the game loop function is a high-order function that should only be executing other functions
 // 4. create base case to end loop once all of one player's ships have been sunk in the game module
-import { Gameboard } from "./gameboard.js";
-import { Player } from "./player.js";
 export function Game() {
-  const player1 = Player();
-  const player2 = Player();
-
-  player1.gameboard = Gameboard();
-  player2.gameboard = Gameboard();
-
-  //place ships
-  player1.gameboard.placeShip([0, 1], [0, 2], [0, 3], [0, 4], [0, 5]);
-  player1.gameboard.placeShip([2, 1], [2, 2], [2, 3], [2, 4]);
-  player1.gameboard.placeShip([3, 2], [4, 2], [5, 2]);
-  player1.gameboard.placeShip([6, 5], [6, 4], [6, 3]);
-  player1.gameboard.placeShip([9, 9], [9, 8]);
-
-  player2.gameboard.placeShip([0, 1], [0, 2], [0, 3], [0, 4], [0, 5]);
-  player2.gameboard.placeShip([5, 1], [5, 2], [5, 3], [5, 4]);
-  player2.gameboard.placeShip([2, 8], [3, 8], [4, 8]);
-  player2.gameboard.placeShip([6, 5], [6, 4], [6, 3]);
-  player2.gameboard.placeShip([8, 9], [8, 8]);
-  player1.turn = true;
-
   function loop(player1, player2, coordinate) {
     const round = {};
     if (player1.turn) {
