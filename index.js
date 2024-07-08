482bc85a9753a6dc98da440764763a92b5005a10
MadebyMarco <marco3xmedina@gmail.com>
2024-07-08 14:45:13 -0700

    fix player2 named as player1

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import { Player } from "./player.js";
 import { UI } from "./UI.js";
 
-const [player1, player2] = [Player(), Player()];
+const [player1, player2] = [Player("player1"), Player("player2")];
 player1.turn = true;
-player2.ai = true;
 UI(player1, player2);
